9f132851cf8b842d75ce66a997395c7be808c88c
rokaia1692006 <rokaia.moh.a@gmail.com>
2025-09-25 21:16:59 +0300

    products + nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AUTH_ROUTES } from './modules/auth/auth.routes';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
+import { USER_ROUTES } from './modules/user/user.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+// get layout then get children routes from module routes
+// auth/login -> auth module routes
+  {path:"",
+    loadChildren:()=>import("./modules/auth/auth.routes").then(m=>m.AUTH_ROUTES)
+  },
+
+  {path:"",
+    loadChildren:()=>import("./modules/user/user.routes").then(m=>m.USER_ROUTES)
+  },
+  {path:"**",loadComponent:()=>import("./shared/components/not-found/not-found.component").then(m=>m.NotFoundComponent), title:"Not Found"}
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+@NgModule({
+  imports: [MatIconModule]
+})
+export class AppModule { }
